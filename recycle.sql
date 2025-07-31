6f512e295f4cd0afa9dcd871f6044ffd688d5cfe
Gobidasan M <gobi.py@gmail.com>
2025-07-31 07:46:06 +0530

    Create recycle.sql

---
+++ recycle.sql
@@ -0,0 +1,3 @@
+drop table table_name; -- drop table is used to temporay earse the table from database and it store in recycle bin
+select * from recyclebin;-- drop tables are store in recyclebin;
+purge table table_name ;--purge is completely delete the table from recyclebin
